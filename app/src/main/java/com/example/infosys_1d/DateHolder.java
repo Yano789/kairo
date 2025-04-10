3d22282c01768244d8eace812cdcaae5e4d0a7ac
Amanda Gladys Bachtiar <127319479+pollygogo@users.noreply.github.com>
2025-04-10 12:52:39 +0800

    fixed disappearing bug but now can't scroll

--- app/src/main/java/com/example/infosys_1d/DateHolder.java
+++ app/src/main/java/com/example/infosys_1d/DateHolder.java
@@ -1,4 +1,19 @@
 package com.example.infosys_1d;
 
+import java.time.LocalDate;
+
 public class DateHolder {
+    private LocalDate Date;
+
+    public DateHolder(LocalDate date) {
+        this.Date = date;
+    }
+
+    public LocalDate getDate() {
+        return Date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.Date = date;
+    }
 }
